d5e6864ef7ee68ecc89dcc2546c4b173a232c879
e <1091139789@qq.com>
2020-06-29 20:11:10 +0800

    增加state

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import Vuex from 'vuex'
 Vue.config.productionTip = false
 
 Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  }
+})
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
